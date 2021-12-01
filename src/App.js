bee1fb593a03b203bc392d79638c781b7609f468
Diego <diegodbonis@gmail.com>
2021-12-01 11:52:10 -0300

    update logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-// import { useState, useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-
-// import {
-//   fetchAllPosts,
-//   selectAllPosts,
-//   selectPostByTitle,
-// } from "./features/Posts/postsSlice";
-
 import { useGetPostsQuery, useGetUsersQuery } from "./services/api";
 
 import Posts from "./features/Posts/Posts";
 
 function App() {
-  // const dispatch = useDispatch();
-
-  // const posts = useSelector(selectAllPosts);
   useGetPostsQuery();
   useGetUsersQuery();
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { api } from "../services/api";
-// import postsReducer from "../features/Posts/postsSlice";
 
 export const store = configureStore({
   reducer: {
-    // posts: postsReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
