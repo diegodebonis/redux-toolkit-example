57cb5abc9105037c4d9b7ed0ac0640f16c066be5
Diego <diegodbonis@gmail.com>
2021-12-01 11:51:38 -0300

    updated logic

--- src/features/Posts/postsSlice.js
+++ src/features/Posts/postsSlice.js
@@ -1,9 +1,4 @@
-import axios from "axios";
-import {
-  createSlice,
-  createAsyncThunk,
-  createSelector,
-} from "@reduxjs/toolkit";
+import { createSelector } from "@reduxjs/toolkit";
 
 import { api } from "../../services/api";
 
@@ -24,33 +19,3 @@ export const selectPostByTitle = createSelector(
   (state, title) => title,
   (posts, title) => posts.filter((item) => item.title.includes(title))
 );
-
-const initialState = {
-  posts: [],
-};
-
-export const fetchAllPosts = createAsyncThunk("/posts", async () => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  return response.data;
-});
-
-export const postsSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {
-    addPost: (state, action) => {
-      state = [...state, action.payload];
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(fetchAllPosts.fulfilled, (state, action) => {
-      state.posts.push(action.payload);
-    });
-  },
-});
-
-export const { addPost } = postsSlice.actions;
-
-export default postsSlice.reducer;
