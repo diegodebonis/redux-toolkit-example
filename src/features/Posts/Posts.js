3de526c23703215ccdb47408cbbd41ab30c3afd2
Diego <diegodbonis@gmail.com>
2021-12-02 23:07:57 -0300

    updated mutations and server

--- src/features/Posts/Posts.js
+++ src/features/Posts/Posts.js
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { selectPostByTitle } from "./postsSlice";
-import { useGetPostsQuery } from "../../services/api";
+import { useGetPostsQuery, useCreatePostMutation } from "../../services/api";
 
 import Post from "./Post";
 
 export default function Posts() {
   const { isLoading, isError, isSuccess, error } = useGetPostsQuery();
+
+  const [createPost, { isLoading: isCreating }] = useCreatePostMutation();
 
   const [filter, setFilter] = useState("");
 
@@ -29,6 +31,15 @@ export default function Posts() {
 
   return (
     <div className="posts">
+      {isCreating ? (
+        "Creating..."
+      ) : (
+        <button
+          onClick={() => createPost({ title: "fouo", body: "baur", userId: 1 })}
+        >
+          Create Post
+        </button>
+      )}
       <h2>Posts</h2>
       Filter:{" "}
       <input
--- src/services/api.js
+++ src/services/api.js
@@ -3,11 +3,26 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://jsonplaceholder.typicode.com/",
+    baseUrl: "http://localhost:3004",
   }),
+  tagTypes: ["Posts", "User"],
   endpoints: (builder) => ({
     getPosts: builder.query({
-      query: () => "/posts",
+      // query: () => "/posts",
+      query: () => "posts",
+      // Provides a list of `Posts` by `id`.
+      // If any mutation is executed that `invalidate`s any of these tags, this query will re-run to be always up-to-date.
+      // The `LIST` id is a "virtual id" we just made up to be able to invalidate this query specifically if a new `Posts` element was added.
+      providesTags: (result) =>
+        // is result available?
+        result
+          ? // successful query
+            [
+              ...result.map(({ id }) => ({ type: "Posts", id })),
+              { type: "Posts", id: "LIST" },
+            ]
+          : // an error occurred, but we still want to refetch this query when `{ type: 'Posts', id: 'LIST' }` is invalidated
+            [{ type: "Posts", id: "LIST" }],
     }),
     getPost: builder.query({
       query: (postId) => `/posts/${postId}`,
@@ -18,6 +33,16 @@ export const api = createApi({
     getUser: builder.query({
       query: (userId) => `/users/${userId}`,
     }),
+    createPost: builder.mutation({
+      query: (data) => {
+        return {
+          url: `/posts`,
+          method: "POST",
+          body: data,
+        };
+      },
+      invalidatesTags: ["Posts"],
+    }),
   }),
 });
 
@@ -26,4 +51,5 @@ export const {
   useGetPostQuery,
   useGetUsersQuery,
   useGetUserQuery,
+  useCreatePostMutation,
 } = api;
